126d388c8d587222978b2b4321b19f33c39a7672
srikanth-io <info.srikanthsankar@gmail.com>
2024-07-21 21:36:12 +0530

    updated

--- Fundamentals/.java/dowhileLoop.java
+++ Fundamentals/.java/dowhileLoop.java
@@ -1,3 +1,10 @@
 public class dowhileLoop {
-    
+    public static void main(String[] args) {
+        int i = 10;
+
+        do{
+            System.out.println("Normal Printing Statement");
+            i++;
+        }while(i <= 20);
+    }
 }
