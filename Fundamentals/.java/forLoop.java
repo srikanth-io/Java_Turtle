d023e02e202247280f3d5558a0481610ff48c20d
srikanth-io <info.srikanthsankar@gmail.com>
2024-07-21 21:22:11 +0530

    updated

---
+++ Fundamentals/.java/forLoop.java
@@ -0,0 +1,10 @@
+
+public class forLoop {
+    public static void main(String[] args) {
+        int age =13;
+
+        for(; age<=30; age++){
+            System.out.println("My age is " + age );
+        }
+    }
+}
